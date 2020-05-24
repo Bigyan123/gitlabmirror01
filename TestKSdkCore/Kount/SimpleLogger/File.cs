87b3dabdb50601af9bd945e9d10c318283733fb9
Bigyan Test <bigyantest@gmail.com>
2020-05-24 06:21:38 +0000

    Update File.cs

--- TestKSdkCore/Kount/SimpleLogger/File.cs
+++ TestKSdkCore/Kount/SimpleLogger/File.cs
@@ -89,7 +89,7 @@ namespace Kount.SimpleLogger
 
             var builder = new ConfigurationBuilder()
                 .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: false);
 
             IConfigurationRoot configuration = builder.Build();
 
