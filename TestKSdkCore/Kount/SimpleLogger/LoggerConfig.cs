1955d3bf54ed1ed720ac57097f9c2f640f451b1f
Bingyan Sahoo <bingyan.sahoo@intimetec.com>
2020-06-02 10:07:32 +0530

    changes

---
+++ TestKSdkCore/Kount/SimpleLogger/LoggerConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace TestKSdkCore.Kount.SimpleLogger
+{
+    public class LoggerConfig
+    {
+        private LoggerConfig(string C)
+        {
+        }
+
+        //    private void logAppend()
+        //    {
+        //       public static readonly log4net.ILog log = log4net.LogManager.GetLogger(typeof(File));
+        //}
+    }
+}
